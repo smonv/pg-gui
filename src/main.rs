b0331123622203d153f64e52a315938c3379ee33
Thanh Van <thanh@smonv.com>
2022-05-27 08:21:25 +0700

    feat(layout): init basic layout

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-use egui::{popup, Button, CentralPanel, Id, TopBottomPanel, Window};
+use egui::{
+    panel::{Side, TopBottomSide},
+    popup, Button, CentralPanel, Id, SidePanel, TopBottomPanel, Window,
+};
+use egui_extras::{Size, TableBuilder};
 
 #[derive(Clone)]
-struct App {
-    pub win_open: bool,
-}
+struct App {}
 
 impl Default for App {
     fn default() -> Self {
-        Self { win_open: true }
+        App {}
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-        TopBottomPanel::new(egui::panel::TopBottomSide::Top, "menu").show(ctx, |ui| {
+        TopBottomPanel::new(TopBottomSide::Top, "menu").show(ctx, |ui| {
             ui.menu_button("Main", |ui| {
                 ui.menu_button("Sub Main", |ui| {
                     if ui.button("Close").clicked() {
@@ -22,19 +24,43 @@ impl eframe::App for App {
                 })
             })
         });
-
-        let mut win_open = true;
-
-        if self.win_open {
-            Window::new("Connection Panel")
-                .open(&mut win_open)
-                .show(ctx, |ui| {
-                    ui.label("text");
-                    if ui.button("Close").clicked() {
-                        self.win_open = false
-                    }
+        SidePanel::new(Side::Left, "side").show(ctx, |ui| {
+            ui.vertical_centered_justified(|ui| {
+                ui.add(Button::new("Conn 1"));
+                ui.add(Button::new("Conn 2"));
+                ui.add(Button::new("Conn 3"));
+                ui.add(Button::new("+"));
+            });
+        });
+        CentralPanel::default().show(ctx, |ui| {
+            TopBottomPanel::new(TopBottomSide::Top, "main-menu").show(ui.ctx(), |ui| {
+                ui.horizontal_wrapped(|ui| {
+                    let mut checked = false;
+                    ui.checkbox(&mut checked, "Field enable");
+                    ui.label("Field");
+                    ui.label("Input")
                 });
-        }
+                ui.horizontal_wrapped(|ui| {
+                    ui.label("Field");
+                    ui.label("Input")
+                });
+            });
+
+            CentralPanel::default().show(ui.ctx(), |ui| {
+                TableBuilder::new(ui)
+                    .column(Size::remainder())
+                    .column(Size::remainder())
+                    .header(30., |mut header| {
+                        header.col(|ui| {
+                            ui.heading("id");
+                        });
+                        header.col(|ui| {
+                            ui.heading("name");
+                        });
+                    })
+                    .body(|_| {});
+            });
+        });
     }
 }
 
